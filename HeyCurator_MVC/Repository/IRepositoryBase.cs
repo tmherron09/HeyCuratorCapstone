030de1253da1ed51c8d503f3af1287ae2b43b30e
Timothy M Herron <tmherron09@gmail.com>
2021-03-11 23:38:04 -0600

    Added IRepository and RepositoryBase to Reduce Scoped Services and repition

---
+++ HeyCurator_MVC/Repository/IRepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HeyCurator_MVC.Repository
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        IQueryable<T> FindAll();
+        IQueryable<T> FindAllBy(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Save();
+        Task SaveAsync();
+        void Delete(T entity);
+
+
+    }
+}
---
+++ HeyCurator_MVC/Repository/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using HeyCurator_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HeyCurator_MVC.Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private ApplicationDbContext _context;
+
+        public RepositoryBase(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll() =>
+            _context.Set<T>().AsNoTracking();
+
+        public IQueryable<T> FindAllBy(Expression<Func<T, bool>> expression) =>
+            _context.Set<T>().Where(expression).AsNoTracking();
+
+        public void Add(T entity) =>
+            _context.Set<T>().Add(entity);
+
+        public async Task AddAsync(T entity) =>
+            await _context.Set<T>().AddAsync(entity);
+
+        public void Update(T entity) =>
+           _context.Set<T>().Update(entity);
+
+        public void Save() =>
+            _context.SaveChanges();
+
+        public async Task SaveAsync() =>
+            await _context.SaveChangesAsync();
+
+
+        public void Delete(T entity) =>
+            _context.Set<T>().Remove(entity);
+
+    }
+}
