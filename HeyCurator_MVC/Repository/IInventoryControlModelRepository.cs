ebcec8eb3587820af5f35424a1aff04f7ef4be8e
Timothy M Herron <tmherron09@gmail.com>
2021-07-09 00:30:17 -0500

    Update InvConModel- Interface/Init implementations.

--- HeyCurator_MVC/Repository/IInventoryControlModelRepository.cs
+++ HeyCurator_MVC/Repository/IInventoryControlModelRepository.cs
@@ -8,8 +8,18 @@ namespace HeyCurator_MVC.Repository
 {
     public interface IInventoryControlModelRepository : IRepositoryBase<InventoryControlModel>
     {
+        Task<InventoryControlModel> GetInventoryControlModel(int icmId);
+        Task<InventoryControlModel> GetInventoryControlModelOfItemInstance(int itemInstanceId);
+        Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfItem(int itemId);
+        Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfEmployee(int employeeId);
+        Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfCuratorSpace(int curatorSpaceId);
+        Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfExhibit(int exhibitId);
+        Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfStorage(int storageId);
 
-
+        Task<InventoryControlModel> GetInvConModWithRecords(int icmId);
+        Task<InventoryControlModel> GetInvConModWithRecentRecords(int icmId, int amount);
+        Task<InventoryControlModel> GetInvConModWithRecordsForTimeSpan(int icmId, TimeSpan timespan);
+        
 
     }
 }
--- HeyCurator_MVC/Repository/InventoryControlModelRepository.cs
+++ HeyCurator_MVC/Repository/InventoryControlModelRepository.cs
@@ -1,5 +1,6 @@
 ﻿using HeyCurator_MVC.Data;
 using HeyCurator_MVC.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,5 +16,76 @@ namespace HeyCurator_MVC.Repository
         {
         }
 
+        public async Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfCuratorSpace(int curatorSpaceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfExhibit(int exhibitId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfItem(int itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<InventoryControlModel>> GetAllInvConModOfStorage(int storageId)
+        {
+            return await Task.Run(() => 
+                _context.StorageItemInstances
+                .Where(sii => sii.StorageId == storageId)
+                .Select(sii => sii.ItemInstance)
+                .Include(ii => ii.InventoryControlModel)
+                .Select(ii => ii.InventoryControlModel));
+        }
+
+        public async Task<InventoryControlModel> GetInvConModWithRecentRecords(int icmId, int amount)
+        {
+            var icm = await _context.InventoryControlModels
+                .Where(icm => icm.Id == icmId)
+                .FirstOrDefaultAsync();
+            icm.Records = await _context.Records.Where(r => r.InventoryControlModelID == icmId )
+                .OrderByDescending(r=> r.TimeStamp)
+                .Take<Record>(amount)
+                .ToListAsync();
+            return icm;
+        }
+
+        public async Task<InventoryControlModel> GetInvConModWithRecords(int icmId)
+        {
+            return await _context.InventoryControlModels
+                .Where(icm => icm.Id == icmId)
+                .Include(icm=> icm.Records)
+                .FirstOrDefaultAsync();
+
+        }
+
+        public async Task<InventoryControlModel> GetInvConModWithRecordsForTimeSpan(int icmId, TimeSpan timespan)
+        {
+            var icm = await _context.InventoryControlModels
+                .Where(icm => icm.Id == icmId)
+                .FirstOrDefaultAsync();
+            var dateComparer = DateTime.Now - timespan;
+            icm.Records = await _context.Records.Where(r => r.InventoryControlModelID == icmId && r.TimeStamp >= dateComparer).ToListAsync();
+            return icm;
+            
+        }
+
+        public async Task<InventoryControlModel> GetInventoryControlModel(int icmId)
+        {
+            return await _context.InventoryControlModels.Where(icm => icm.Id == icmId).FirstOrDefaultAsync();
+        }
+
+        public async Task<InventoryControlModel> GetInventoryControlModelOfItemInstance(int itemInstanceId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
