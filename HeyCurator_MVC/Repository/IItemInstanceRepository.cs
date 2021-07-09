6fdd0bd52cb02c0237c7f9dcbebac0edcca8877c
Timothy M Herron <tmherron09@gmail.com>
2021-07-09 00:29:42 -0500

    Update IItemInstanceRepo -Init ItemInstanceRepo with null implementations.

--- HeyCurator_MVC/Repository/IItemInstanceRepository.cs
+++ HeyCurator_MVC/Repository/IItemInstanceRepository.cs
@@ -8,7 +8,16 @@ namespace HeyCurator_MVC.Repository
 {
     public interface IItemInstanceRepository : IRepositoryBase<ItemInstance>
     {
+        Task<ItemInstance> GetItemInstanceBase(int itemInstanceId);
+        Task<ItemInstance> GetItemInstanceWithStorages(int itemInstanceId);
+        Task<ItemInstance> GetItemInstanceWithExhibits(int itemInstanceId);
+        Task<ItemInstance> GetItemInstanceWithStoragesAndExhibits(int itemInstanceId);
+        Task<IEnumerable<ItemInstance>> GetAllItemInstanceBase();
+        Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithStorages();
+        Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithExhibits();
+        Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithStoragesAndExhibits();
 
+        
 
 
     }
--- HeyCurator_MVC/Repository/ItemInstanceRepository.cs
+++ HeyCurator_MVC/Repository/ItemInstanceRepository.cs
@@ -15,5 +15,44 @@ namespace HeyCurator_MVC.Repository
         {
         }
 
+        public Task<IEnumerable<ItemInstance>> GetAllItemInstanceBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithExhibits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithStorages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ItemInstance>> GetAllItemInstanceBaseWithStoragesAndExhibits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemInstance> GetItemInstanceBase(int itemInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemInstance> GetItemInstanceWithExhibits(int itemInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemInstance> GetItemInstanceWithStorages(int itemInstanceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ItemInstance> GetItemInstanceWithStoragesAndExhibits(int itemInstanceId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
