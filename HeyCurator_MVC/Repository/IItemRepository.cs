451d9a8189646ece85ddd13c463c0fc46b55681d
Timothy M Herron <tmherron09@gmail.com>
2021-07-08 22:46:57 -0500

    Update- IItemRepository Populated

--- HeyCurator_MVC/Repository/IItemRepository.cs
+++ HeyCurator_MVC/Repository/IItemRepository.cs
@@ -8,7 +8,29 @@ namespace HeyCurator_MVC.Repository
 {
     public interface IItemRepository : IRepositoryBase<Item>
     {
+        /// <summary>
+        /// Returns Item Model without references.
+        /// </summary>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        Task<Item> GetItemBase(int itemId);
+        Task<Item> GetItemWithInstances(int itemId);
+        Task<Item> GetItemWithInstancesWithStorages(int itemId);
+        Task<Item> GetItemWithInstancesWithExhibits(int itemId);
+        Task<Item> GetItemWithInstancesWithStoragesAndExhibits(int itemId);
 
+        Task<IEnumerable<Item>> GetAllItemsBase();
+        Task<IEnumerable<Item>> GetAllItemsWithInstances();
+        Task<IEnumerable<Item>> GetAllItemsWithInstancesWithStorages();
+        Task<IEnumerable<Item>> GetAllItemsWithInstancesWithExhibits();
+        Task<IEnumerable<Item>> GetAllItemsWithInstancesWithStoragesAndExhibits();
+
+        Task PopulateItemBaseWithInstances(Item item);
+        Task PopulateInstancesWithStorages(ItemInstance itemInstance);
+        Task PopulateInstancesWithExhibits(ItemInstance itemInstance);
+        Task PopulateAllItemInstancesWithStorages(Item item);
+        Task PopulateAllItemInstancesWithExhibits(Item item);
+        Task PopulateAllItemInstancesWithStoragesAndExhibits(Item item);
 
 
     }
